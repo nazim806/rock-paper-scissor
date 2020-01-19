37dc4d812e5ca3e2e904832d3d05b0a0f80782e7
Nazim Uddin <umnazim@gmail.com>
2020-01-19 11:25:05 -0800

    javascript functions created

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
 
+  //start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  // play match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    // computer options
+  };
+  // call all the inner function
+  startGame();
+};
+
+//start the game function
+
+game();
